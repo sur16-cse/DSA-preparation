f40c13c86777b49ae0625cc9f77510f3bfb01dfe
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-04-29 11:49:55 +0530

    Time: 128 ms (29.97%), Space: 44.3 MB (6.20%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -7,7 +7,7 @@ public:
         int ans=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(m.find(sum-k)!=m.end())
+            //if(m.find(sum-k)!=m.end())
                 ans+=m[sum-k];
             m[sum]++;
         }
