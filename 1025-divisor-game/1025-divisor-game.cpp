b59d1d52c3b2fee0b68c70366cbbdd86836c8385
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-05-10 11:00:53 +0530

    Time: 5 ms (19.71%), Space: 6 MB (35.99%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -4,7 +4,7 @@ public:
     bool divisorGame(int n) {
       if(n==1) return 0;
         if(dp[n]>0) return dp[n];
-        for(int i=1;i<n;i++){
+        for(int i=1;i*i<n;i++){
             if(n%i==0)
                 //next player loses the game
                 if(divisorGame(n-i)==0)return dp[n]=1;
