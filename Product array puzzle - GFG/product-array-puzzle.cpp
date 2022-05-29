440c11a3a10f2c16ef8f1f5cfa3e787ab4958a6a
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-05-29 15:33:33 +0530

    Added solution - LeetHub

--- Product array puzzle - GFG/product-array-puzzle.cpp
+++ Product array puzzle - GFG/product-array-puzzle.cpp
@@ -13,19 +13,57 @@ class Solution{
   public:
     // nums: given vector
     // return the Product vector P that hold product except self at each index
-    vector<long long int> productExceptSelf(vector<long long int>& arr, int n) {
-       //code here 
-       vector<long long int>out;
-      
-       for(int i=0;i<n;i++){
-            long long int mul=1;
-            for(int j=0;j<n;j++){
-                if(i!=j)
-                mul*=arr[j];
-            }
-            out.push_back(mul);
-       }
-       return out;
+    vector<long long int> productExceptSelf(vector<long long int>& a, int n) {
+      long prod = 1;
+    long flag = 0;
+
+    // product of all elements
+    for (int i = 0; i < n; i++) {
+
+        // counting number of elements
+        // which have value
+        // 0
+        if (a[i] == 0)
+            flag++;
+        else
+            prod *= a[i];
+    }
+
+    // creating a new array of size n
+   
+   vector<long long int>arr(n);
+
+    for (int i = 0; i < n; i++) {
+
+        // if number of elements in
+        // array with value 0
+        // is more than 1 than each
+        // value in new array
+        // will be equal to 0
+        if (flag > 1) {
+            arr[i] = 0;
+        }
+
+        // if no element having value
+        // 0 than we will
+        // insert product/a[i] in new array
+        else if (flag == 0)
+            arr[i] = (prod / a[i]);
+
+        // if 1 element of array having
+        // value 0 than all
+        // the elements except that index
+        // value , will be
+        // equal to 0
+        else if (flag == 1 && a[i] != 0) {
+            arr[i] = 0;
+        }
+
+        // if(flag == 1 && a[i] == 0)
+        else
+            arr[i] = prod;
+    }
+    return arr;
     }
 };
 
