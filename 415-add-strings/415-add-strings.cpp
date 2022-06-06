c060637a31964a47198f05d5ed53e6bef35a45ca
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-06-06 09:10:23 +0530

    Time: 6 ms (64.04%), Space: 6.7 MB (89.11%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i=num1.size()-1,j=num2.size()-1;
+        int carry=0;
+        string ans="";
+        while(i>=0 || j>=0 || carry>0){
+            if(i>=0)carry+=(num1[i--]-'0');
+            if(j>=0)carry+=(num2[j--]-'0');
+            ans+=char(carry%10+'0');
+            carry=carry/10;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
