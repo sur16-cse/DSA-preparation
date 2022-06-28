f238a1d9de2277ac417dbf7a7db535cbbaf2b48f
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-06-28 21:35:40 +0530

    Added solution - LeetHub

--- Implement strstr - GFG/implement-strstr.cpp
+++ Implement strstr - GFG/implement-strstr.cpp
@@ -38,4 +38,20 @@ int strstr(string haystack, string needle) {
                    return i;
         }
         return -1;
+        
     }
+    /*int strstr(string s, string x)
+{
+    int i,n;
+    n = s.length();
+    int m = x.length();
+    for(i=0;i<n;i++)
+    {
+       string req  = s.substr(i,m);
+       if(req==x)
+       {
+           return i;
+       }
+    }
+    return -1;
+}*/
