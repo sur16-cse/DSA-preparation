68c7bf3f10d5e4d4cfbae61259532dbf0ce6530d
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-07-21 22:19:58 +0530

    Time: 25 ms (44.20%), Space: 12.1 MB (33.43%) - LeetHub

---
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int>ans;
+    void helper(int i,int n)
+    {
+        if(i>n)
+             return;
+        ans.push_back(i);
+        for (int j = 0; j <= 9; ++j)
+        {
+           helper(10*i+j,n);
+        }
+    }
+    vector<int> lexicalOrder(int n) 
+	{
+        for(int i=1;i<=9;i++)
+            helper(i,n);
+        return ans;
+    }
+};
